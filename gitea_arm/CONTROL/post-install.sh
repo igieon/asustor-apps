ea2eed66b707d3f8414e3dc2916c76314526acc7
David Ignjic <ignjic@gmail.com>
2020-06-19 14:03:49 +0200

    Gitea 1.12.0 arm, update description, changelog

---
+++ gitea_arm/CONTROL/post-install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+docker run -itd --name=Gitea \
+	--restart=unless-stopped \
+	-p 10022:22 -p 13000:3000 \
+	-v /volume1/gitea_data:/data \
+	gitea/gitea:1.12.0
+
+
+case "$APKG_PKG_STATUS" in
+	install)
+		;;
+	upgrade)
+		;;
+	*)
+		;;
+esac
+exit 0
